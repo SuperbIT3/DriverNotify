98b1482e1da93e53ee41eae565566d8a055ad9a2
SuperbIT3 <it3@superbhyper.co.za>
2024-10-16 15:56:59 +0200

    initinit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
